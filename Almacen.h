fa91320a99f98542fee2ce2f3b447e6f10307876
Alex <fav09@protonmail.com>
2021-12-22 01:01:09 -0500

    feat: Añadir el metodo virtual add_register para crear registros de un producto, y añadir constructor vacio

--- Almacen.h
+++ Almacen.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//To-Do : Buscar libreria para formatear fecha y hacer Date clase padre
 class Date {    //clase para almacenar la fecha
 public:
     int day;
@@ -32,7 +33,7 @@ public:
     string descripcion;
     string paisOrigen;
     int cantidad;
-
+    RegistroProducto();
     RegistroProducto(string _nombre, long _codigo, string _descripcion, string _paisOrigen, int _cantidad) {
         nombre = _nombre;
         codigo = _codigo;
@@ -42,7 +43,13 @@ public:
 
     }
 
-    //virtual void agregar();
+    virtual void add_register(){
+        cout<<"Introduzca el nombre del producto: ";cin>>this->nombre;
+        cout<<"Introduzca el codigo del producto: ";cin>>this->codigo;
+        cout<<"Introduzca la descripcion del producto: ";cin>>this->descripcion;
+        cout<<"Introduzca el pais de origen del producto: ";cin>>this->paisOrigen;
+        cout<<"Introduzca la cantidad de elementos que va a tener el producto: ";this->cantidad;
+    };
     // virtual void eliminar();
     //virtual void imprimir();
 
@@ -55,8 +62,9 @@ public:
     char talla;
 
     RegistroTextiles(string _nombre, long _codigo, string _descripcion, string _paisOrigen, int _cantidad,
-                     string _material, char _sexo, char _talla) : RegistroProducto(_nombre, _codigo, _descripcion, _paisOrigen,
-                                                                           _cantidad) {
+                     string _material, char _sexo, char _talla) : RegistroProducto(_nombre, _codigo, _descripcion,
+                                                                                   _paisOrigen,
+                                                                                   _cantidad) {
         material = _material;
         sexo = _sexo;
         talla = _talla;
@@ -77,8 +85,9 @@ public:
     bool manual;
 
     RegistroElectrodomesticos(string _nombre, long _codigo, string _descripcion, string _paisOrigen, int _cantidad,
-                              int _tiempo, int _voltaje, bool _manual) : RegistroProducto(_nombre, _codigo, _descripcion, _paisOrigen,
-                                                                                  _cantidad) {
+                              int _tiempo, int _voltaje, bool _manual) : RegistroProducto(_nombre, _codigo,
+                                                                                          _descripcion, _paisOrigen,
+                                                                                          _cantidad) {
         tiempo = _tiempo;
         voltaje = _voltaje;
         manual = _manual;
@@ -101,10 +110,11 @@ public:
     long codigoEstiba;
     bool eliminado;
 
-    RegistroAlimentos(string _nombre, long _codigo, string _descripcion, string _paisOrigen, int _cantidad, int _dia, int _mes,
+    RegistroAlimentos(string _nombre, long _codigo, string _descripcion, string _paisOrigen, int _cantidad, int _dia,
+                      int _mes,
                       int _anio, string _clasificacion, string _empleado, long _codigoEstiba,
                       bool _eliminado) : RegistroProducto(_nombre, _codigo, _descripcion, _paisOrigen, _cantidad),
-                                 Date(_dia, _mes, _anio) {
+                                         Date(_dia, _mes, _anio) {
         clasificacion = _clasificacion;
         empleado = _empleado;
         codigoEstiba = _codigoEstiba;
