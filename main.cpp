50f9e78b5506959ac96c145f28b6498b555301e8
Alex <fav09@protonmail.com>
2021-12-22 00:23:58 -0500

    feat: Añadir modelo basico de menu

--- main.cpp
+++ main.cpp
@@ -9,8 +9,99 @@ using namespace std;
 const string almacen_text = "textiles.dat";
 const string almacen_electrodomesticos = "electrodomesticos.dat";
 const string almacen_alimentos = "alimentos.dat";
+void main_menu();
+void sub_menu_a();
+void sub_menu_b();
+void function_a();
+void function_b();
+void function_c();
 
-int main() {
+void main_menu()
+{
+    cout << "You are at main menu." << endl;
 
+    while(true)
+    {
+        cout << "Select one of the following options:" << endl;
+        cout << "1 -> Sub menu A" << endl;
+        cout << "2 -> Sub menu B" << endl;
+
+        int option;
+
+        cin >> option;
+        switch(option)
+        {
+            case 1:
+                sub_menu_a();
+                break;
+            case 2:
+                sub_menu_b();
+                break;
+            default:
+                cout << "Please insert an integer between 1 and 2." << endl;
+                break;
+        }
+    }
+}
+
+void sub_menu_a()
+{
+    cout << "You are at sub menu A." << endl;
+
+    while(true)
+    {
+        cout << "Select one of the following options:" << endl;
+        cout << "1 -> Function A" << endl;
+        cout << "2 -> Function B" << endl;
+        cout << "3 -> Back to main menu" << endl;
+
+
+        int option;
+
+        cin >> option;
+        switch(option)
+        {
+            case 1:
+                function_a();
+                break;
+            case 2:
+                function_b();
+                break;
+            case 3:
+                main_menu();
+                break;
+            default:
+                cout << "Please insert an integer between 1 and 3." << endl;
+                break;
+        }
+    }
+}
+
+void sub_menu_b() {
+    cout << "You are at sub menu B." << endl;
+
+    while (true) {
+        cout << "Select one of the following options:" << endl;
+        cout << "1 -> Function C" << endl;
+        cout << "2 -> Back to main menu" << endl;
+
+
+        int option;
+
+        cin >> option;
+        switch (option) {
+            case 1:
+                function_c();
+                break;
+            case 2:
+                main_menu();
+                break;
+            default:
+                cout << "Please insert an integer between 1 and 2." << endl;
+                break;
+        }
+    }
+}
+int main(){
     return 0;
 }
