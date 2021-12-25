7184e6662f0d8bda2ba19de65a61cb3cf46b6420
Yorny2101 <yornyf3@nauta.cu>
2021-12-25 16:15:36 -0500

    feat: implementacion de los metodos find, extract y add para las clases containers, solo falta el trabajo con la fecha para los alimentos y el manejo de excepciones en la entrada de datos por parte del usuario

--- Containers.h
+++ Containers.h
@@ -17,7 +17,6 @@ public:
     {
         if(counter == size_of_array)
         {
-            cout << "Extending array..." << endl;
 
             size_of_array *= 2;
             RegistroTextiles** new_array = new RegistroTextiles*[size_of_array];
@@ -30,26 +29,114 @@ public:
             delete [] in_memory_warehouse;
             in_memory_warehouse = new_array;
         }
-        else
-        {
-            cout << "Added without extending." << endl;
-        }
+
 
         in_memory_warehouse[counter] = new_entry;
         counter++;
+    }
+    void extract(int index, int cant)
+    {  if(index == -1){
+        cout << "Producto no encontrado\n";
+    }
+        else if(cant > in_memory_warehouse[index]->cantidad){
+        cout << "Esa cantidad no esta disponible en el almacen\n";
+
+    }
+        else if(cant < in_memory_warehouse[index]->cantidad){
+            in_memory_warehouse[index]->cantidad -= cant;
+    }
+        else{
+            counter--;
+
+            for(int i = index; i < counter; i++)
+            {
+                in_memory_warehouse[i] = in_memory_warehouse[i+1];
+            }
+       }
+
+    }
+    int find_index(string name)
+    { /*debe tenerse en cuenta que el usuario puede emplera lo mismo mayuscuka q minucula por lo q es
+ conveniente cambiar la entrada del usuario ya sea todo en mayuscula o en minuysculas*/
+        for(int i = 0; i < counter; i++)
+        {
+            if(in_memory_warehouse[i]->nombre == name)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 };
 
 class ContainerElectrodomesticos {
 public:
-    int size_of_array;
-    int counter;
+    int size_of_array = 4;
+    int counter = 0;
     RegistroElectrodomesticos **in_memory_warehouse;
 
-    ContainerElectrodomesticos(int size_of_array, int counter) {
+    ContainerElectrodomesticos(){
+        in_memory_warehouse = new RegistroElectrodomesticos*[size_of_array];
+    }
+
+    /*ContainerElectrodomesticos(int size_of_array, int counter) {
         this->size_of_array = size_of_array;
         this->counter = counter;
         in_memory_warehouse = new RegistroElectrodomesticos*[size_of_array];
+    }*/
+    /*Los parametros deben quedar inicializados, esta operacion no puede depender del usuario*/
+    void add(RegistroElectrodomesticos* new_entry)
+    {
+        if(counter == size_of_array)
+        {
+            size_of_array *= 2;
+            RegistroElectrodomesticos** new_array = new RegistroElectrodomesticos*[size_of_array];
+
+            for(int i = 0; i < size_of_array/2; i++)
+            {
+                new_array[i] = in_memory_warehouse[i];
+            }
+
+            delete [] in_memory_warehouse;
+            in_memory_warehouse = new_array;
+        }
+
+
+        in_memory_warehouse[counter] = new_entry;
+        counter++;
+    }
+    void extract(int index, int cant)
+    {  if(index == -1){
+            cout << "Producto no encontrado\n";
+        }
+        else if(cant > in_memory_warehouse[index]->cantidad){
+            cout << "Esa cantidad no esta disponible en el almacen\n";
+
+        }
+        else if(cant < in_memory_warehouse[index]->cantidad){
+            in_memory_warehouse[index]->cantidad -= cant;
+        }
+        else{
+            counter--;
+
+            for(int i = index; i < counter; i++)
+            {
+                in_memory_warehouse[i] = in_memory_warehouse[i+1];
+            }
+        }
+
+    }
+    int find_index(string name)
+    { /*debe tenerse en cuenta que el usuario puede emplera lo mismo mayuscuka q minucula por lo q es
+ conveniente cambiar la entrada del usuario ya sea todo en mayuscula o en minuysculas*/
+        for(int i = 0; i < counter; i++)
+        {
+            if(in_memory_warehouse[i]->nombre == name)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 
 };
@@ -60,10 +147,71 @@ public:
     int counter;
     RegistroAlimentos **in_memory_warehouse;
 
-    ContainerAlimentos(int size_of_array, int counter) {
+    /*ContainerAlimentos(int size_of_array, int counter) {
         this->size_of_array = size_of_array;
         this->counter = counter;
         in_memory_warehouse = new RegistroAlimentos*[size_of_array];
-    }
+    }*/
+    void add(RegistroAlimentos* new_entry)
+    {
+        if(counter == size_of_array)
+        {
+            size_of_array *= 2;
+            RegistroAlimentos** new_array = new RegistroAlimentos*[size_of_array];
 
+            for(int i = 0; i < size_of_array/2; i++)
+            {
+                new_array[i] = in_memory_warehouse[i];
+            }
+
+            delete [] in_memory_warehouse;
+            in_memory_warehouse = new_array;
+        }
+
+
+        in_memory_warehouse[counter] = new_entry;
+        counter++;
+    }
+    void extract(int index, int cant) /* se debe poner una condicional para comprobar que el producto esta
+ vencido mediante la fcha de vencimiento*/
+    {  if(index == -1){
+            cout << "Producto no encontrado\n";
+        }
+        else if(cant > in_memory_warehouse[index]->cantidad){
+            cout << "Esa cantidad no esta disponible en el almacen\n";
+
+        }
+        else if(cant <= in_memory_warehouse[index]->cantidad){
+            in_memory_warehouse[index]->cantidad -= cant;
+            cout << "introduzca su nombre para extraer el producto: ";
+           string nombreEmpleado;
+            cin >> nombreEmpleado;
+            in_memory_warehouse[index]->empleado = nombreEmpleado;
+            cout << "Introduzca su codigo de estiba: ";
+            long codigoEstib;
+            cin >> codigoEstib;
+            in_memory_warehouse[index]->codigoEstiba = codigoEstib;
+        }
+        /*else{
+            counter--;
+
+            for(int i = index; i < counter; i++)
+            {
+                in_memory_warehouse[i] = in_memory_warehouse[i+1];
+            }
+        }*/
+
+    }
+    int find_index(string name)
+    { /*debe tenerse en cuenta que el usuario puede emplera lo mismo mayuscuka q minucula por lo q es
+ conveniente cambiar la entrada del usuario ya sea todo en mayuscula o en minuysculas*/
+        for(int i = 0; i < counter; i++)
+        {
+            if(in_memory_warehouse[i]->nombre == name)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 };
