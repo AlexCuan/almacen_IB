ff31a4d56326d9aa0eb645d8ad7129b11c8f9bec
AlexCuan <fav09@protonmail.com>
2022-01-09 23:18:29 -0500

    fix: Limpiar main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@
 using namespace std;
 
 int main() {
-    load_info();
-    //cout << textiles_departament.in_memory_warehouse[0]->nombre;
-   // add_dummies();
-   food_department.in_memory_warehouse[1]->imprimir(cout, COUT);
     main_menu();
 
     return 0;
