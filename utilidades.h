7d197b4c97a9eea6e777e36f63e98f75aac91665
AlexCuan <fav09@protonmail.com>
2022-01-09 23:19:08 -0500

    fix: Cambiar key por enter

--- utilidades.h
+++ utilidades.h
@@ -6,57 +6,51 @@
 #include <limits>
 #include <vector>
 
-
-
-
-
-
 using namespace std;
 
-string getDate()
-{
+string getDate() {
     time_t t = time(NULL);
-    tm* timePtr = localtime(&t);
+    tm *timePtr = localtime(&t);
     stringstream ss_year;
-    ss_year << timePtr->tm_year+1900;
+    ss_year << timePtr->tm_year + 1900;
     string Year = ss_year.str();
 
     stringstream ss_month;
-    ss_month << timePtr->tm_mon+1;
+    ss_month << timePtr->tm_mon + 1;
     string Month = ss_month.str();
-    if(atoi(Month.c_str()) < 10)
-        Month = "0"+Month;
+    if (atoi(Month.c_str()) < 10)
+        Month = "0" + Month;
 
     stringstream ss_day;
     ss_day << timePtr->tm_mday;
     string Day = ss_day.str();
-    if(atoi(Day.c_str()) < 10)
-        Day = "0"+Day;
+    if (atoi(Day.c_str()) < 10)
+        Day = "0" + Day;
 
     stringstream ss_hour;
     ss_hour << timePtr->tm_hour;
     string Hour = ss_hour.str();
-    if(atoi(Hour.c_str()) < 10)
-        Hour = "0"+Hour;
+    if (atoi(Hour.c_str()) < 10)
+        Hour = "0" + Hour;
 
     stringstream ss_min;
     ss_min << timePtr->tm_min;
     string Min = ss_min.str();
-    if(atoi(Min.c_str()) < 10)
-        Min = "0"+Min;
+    if (atoi(Min.c_str()) < 10)
+        Min = "0" + Min;
 
     stringstream ss_sec;
     ss_sec << timePtr->tm_sec;
     string Sec = ss_sec.str();
-    if(atoi(Sec.c_str()) < 10)
-        Sec = "0"+Sec;
+    if (atoi(Sec.c_str()) < 10)
+        Sec = "0" + Sec;
 
-    string Fecha = Year+Month+Day;//+"_"+Hour+Min+Sec;
+    string Fecha = Year + Month + Day;//+"_"+Hour+Min+Sec;
 
     return Fecha;
 }
 
-int obtainDate(){
+int obtainDate() {
     int date;
     string fecha = getDate();
     date = atoi(fecha.c_str());
@@ -65,13 +59,8 @@ int obtainDate(){
 }
 
 
-
-
-
-
 template<typename T>
-T &validateInput_int(T &val)
-{
+T &validateInput_int(T &val) {
     while (true) {
 
         if (cin >> val && val > 0) {
@@ -86,39 +75,40 @@ T &validateInput_int(T &val)
 }
 
 
-string minusculas(string cadena){
+string minusculas(string cadena) {
 
-    for(int i = 0; i < cadena.length(); i++){
+    for (int i = 0; i < cadena.length(); i++) {
         cadena[i] = tolower(cadena[i]);
     }
     return cadena;
 }
 // hacer una funcion para contraer y otra para extender
 
-string contraer(string cadena){
-    for(int i = 0; i < cadena.size(); i++){
-        if(cadena[i] == 32)
+string contraer(string cadena) {
+    for (int i = 0; i < cadena.size(); i++) {
+        if (cadena[i] == 32)
             cadena[i] = 36;
-    }return cadena;
+    }
+    return cadena;
 }
-string extender(string cadena){
-    for(int i = 0; i < cadena.size(); i++){
-        if(cadena[i] == 36)
+
+string extender(string cadena) {
+    for (int i = 0; i < cadena.size(); i++) {
+        if (cadena[i] == 36)
             cadena[i] = 32;
     }
     return cadena;
 }
 
 
-
 void limpiar_consola() {
     system("clear");
 }
 
-bool stringDigit(string cadena){
+bool stringDigit(string cadena) {
     bool digit = true;
-    for(int i = 0; i < cadena.size(); i++){
-        if(!(isdigit(cadena[i]))){
+    for (int i = 0; i < cadena.size(); i++) {
+        if (!(isdigit(cadena[i]))) {
             digit = false;
             break;
         }
@@ -127,11 +117,11 @@ bool stringDigit(string cadena){
 }
 
 
-string visoi(string cadena){
-    while(true){
-        if(cin >> cadena && stringDigit(cadena))
+string visoi(string cadena) {
+    while (true) {
+        if (cin >> cadena && stringDigit(cadena))
             break;
-        else{
+        else {
             cout << "Enter a valid integer value:\n";
             cin.clear();
             cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
@@ -141,8 +131,7 @@ string visoi(string cadena){
     return cadena;
 }
 
-void pause(){
-    cout << "Press any key to continue ...";
+void pause() {
+    cout << "Press enter to continue ...";
     getchar();
-
 }
